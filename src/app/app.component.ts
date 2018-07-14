e49a3c839a9fe1949b3c70c317aa06d4b0bcbe2a
CalvinMogodi <Calvin.Mogodi@eoh.co.za>
2018-07-14 15:40:42 +0200

    last check in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Email } from '../assets/js/smtp';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'app';
+  sendmail(): void {
+    
+ /*   this.router.send("cmogodi@you.com",
+"cmogodi@gmail.com",
+"This is a subject",
+"this is the body",
+"smtp.gmail.com",
+"cmogodi@gmail.com",
+"mcmogodi",
+function done(message) {
+   alert("sent") 
+  }  );*/
+
+
+  }
 }
+
+
